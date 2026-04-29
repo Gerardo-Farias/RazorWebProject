89756163309882f32d6f98d673341f91a3e35630
Roux <gfarias10@ucol.mx>
2026-04-29 17:10:16 -0600

    Añadí la tarea 16: registro de estudiantes.

---
+++ MiHistoria/Components/Clases/Estudiante.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Estudiante
+{
+    [Required(ErrorMessage = "El nombre no debe estar vacío")]
+    public string Nombre { get; set; }
+
+    [Required(ErrorMessage = "El correo no debe estar vacío")]
+    [EmailAddress(ErrorMessage = "Correo no válido")]
+    public string Correo { get; set; }
+
+    [Required(ErrorMessage = "La calificación no debe estar vacía")]
+    [Range(0, 10, ErrorMessage = "La calificación debe estar entre 0 y 10")]
+    public double? Calificacion { get; set; } = 0;
+}
